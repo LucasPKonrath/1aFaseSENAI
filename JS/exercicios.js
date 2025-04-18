53d0fb44af71bee629c52905633928254d648160
LucasPKonrath <lucaspkonrath32@gmail.com>
2025-04-17 21:03:22 -0300

    Adicionei calculadora de botoes html

---
+++ JS/exercicios.js
@@ -0,0 +1,43 @@
+
+function somar(){
+    let n1, n2, soma
+
+    n1 = Number(prompt("Pimeiro Número: "))
+    n2 = Number(prompt("Segundo Número: "))
+    //n1 = Number(n1)
+    //n2 = Number(n2)
+    soma = n1 + n2
+
+    alert("A soma de "+n1 +" e de " +n2+" é igual a: " + soma)
+}
+
+function diminuir(){
+    let n1, n2, subtracao
+
+    n1 = Number(prompt("Primeiro Número: "))
+    n2 = Number(prompt("Segundo Número: "))
+
+    subtracao = n1 - n2
+
+    alert("A subtração de "+ n1 +" e de " + n2 +" é igual a: " + subtracao)
+}
+function multiplicar(){
+    let n1, n2, multiplicacao
+
+    n1 = Number(prompt("Primeiro Número: "))
+    n2 = Number(prompt("Segundo Número: "))
+
+    multiplicacao = n1 * n2
+
+    alert("A multiplicação de "+ n1 +" e de " + n2 +" é igual a: " + multiplicacao)
+}
+function dividir(){
+    let n1, n2, divisao
+
+    n1 = Number(prompt("Primeiro Número: "))
+    n2 = Number(prompt("Segundo Número: "))
+
+    divisao = n1 / n2
+
+    alert("A divisão de "+ n1 +" e de " + n2 +" é igual a: " + divisao)
+}
