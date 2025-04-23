d9a7f324f7ecd30eaab7b77f36d6f9e16cd658b9
LucasPKonrath <lucaspkonrath32@gmail.com>
2025-04-22 21:54:40 -0300

    Exercicios puxando funções html

---
+++ JS/exercicioHtmlAlex.js
@@ -0,0 +1,22 @@
+function exercicio1(){
+    let num1 = Number(prompt("Digite um número: "))
+    let num2 = Number(prompt("Digite outro número: "))
+    let soma = num1 + num2 
+    alert("A soma dos 2 números é " + soma)
+}
+
+function exercicio2(){
+    let num1 = Number(prompt("Digite um número: "))
+    let num2 = Number(prompt("Digite outro número: "))
+    let media = (num1 + num2) / 2
+    
+    alert (`A média dos dois números é: ${media} `)
+}
+
+function exercicio3(){
+    let num1 = Number(prompt("Digite um número: "))
+    let num2 = Number(prompt("Digite outro número: "))
+    let subtracao = num1 - num2
+    
+    alert("A subtração dos dois números é: " + subtracao)
+}
