9d2fa850b49f902eaa476451a3fc83858e504a43
LucasPKonrath <lucaspkonrath32@gmail.com>
2025-05-29 22:16:57 -0300

    add css cadastro/login

--- CSS/css.js
+++
@@ -1,8 +0,0 @@
-function mostrarlogin(){
-    document.getElementById('login').style.display = 'flex'
-    document.getElementById('cadastro').style.display = 'none'
-}
-function mostrarcadastro(){
-    document.getElementById('login').style.display = 'none'
-    document.getElementById('cadastro').style.display = 'flex'
-}
